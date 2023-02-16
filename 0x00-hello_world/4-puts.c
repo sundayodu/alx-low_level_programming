adce74b3b2896103f68078289479be5802a6d60f
sundayodu <sundayemmanuelng@yahoo.com>
2023-02-16 18:18:02 +0100

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stuio.h>
+/*
+   * main - print using a put function 
+   * Return: the rutun value is 0
+   */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+	{
