ba8092267cf01b438b022c5a537ea1ff7208e517
sundayodu <sundayemmanuelng@yahoo.com>
2023-02-16 14:59:02 -0800

    task 7

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - this prints exactly the input on it
+ * Return: 1
+ */
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
