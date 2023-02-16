dc7c34a65604abdf2b5d81cef81b332dc5b3cdde
sundayodu <sundayemmanuelng@yahoo.com>
2023-02-16 15:12:51 -0800

    task7

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
 #include <unistd.h>
 /**
  * main - this prints exactly the input on it
- * Return: 1
+ * Return: always 1.
  */
 int main(void)
 {
-	(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
 	return (1);
 }
