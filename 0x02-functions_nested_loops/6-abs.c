a8da13bd144cdae34e8de6c32e7d89ade132f188
sundayodu <sundayemmanuelng@yahoo.com>
2023-02-22 14:24:48 +0100

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -6,14 +6,13 @@
  * Return: Absolute value
  */
 int _abs(int c)
-
 {
 	if (c < 0)
 	{
-	int abs_val;
-		
-	abs_val = c * -1;
-	return (abs_val);
+		int abs_val;
+
+		abs_val = c * -1;
+		return (abs_val);
 	}
 	return (c);
 }
