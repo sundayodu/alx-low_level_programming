19edf36286eda78aef99466236d80efea02d54c8
sundayodu <sundayemmanuelng@yahoo.com>
2023-02-22 22:31:41 +0100

    100 times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
   * print_times_table - printsthe times inputed
@@ -16,7 +15,7 @@ void print_times_table(int n)
 	{
 	for (num = 0; num <= n; num++)
 	{
-	putchar('0');
+	_putchar('0');
 	for (mult = 1; mult <= n; mult++)
 	{
 	_putchar(',');
