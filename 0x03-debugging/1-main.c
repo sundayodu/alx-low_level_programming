4e7039e1bcca582483ad544c10bf738ad6388067
sundayodu <sundayemmanuelng@yahoo.com>
2023-02-23 17:07:03 +0100

    1-main.c task

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -11,13 +11,12 @@ int main(void)
 
 	printf("Infinite loop incoming :(\n");
 	/**
-	i = 0;
-
-	while (1 < 10)
-	{
-	putchar(i);
-	}
-	*/
+	* i = 0;
+	* while (1 < 10)
+	* {
+	* putchar(i);
+	* }
+	 */
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
