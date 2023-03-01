ac8e35dc355051df3e38e42b3c14970e63fa2229
sundayodu <sundayemmanuelng@yahoo.com>
2023-03-01 21:28:38 +0100

    5-string_toupper

--- 0x06-pointers_arrays_strings/5-main.c
+++ 0x06-pointers_arrays_strings/5-main.c
@@ -2,16 +2,16 @@
 #include <stdio.h>
 
 /**
- * main - check the code
+ * main - check the code of the program
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	char str[] = "Look up!\n";
+	char str[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\n hello world! hello-world 0123456hello world\thello world.hello world\n";
 	char *ptr;
 
-	ptr = string_toupper(str);
+	ptr = cap_string(str);
 	printf("%s", ptr);
 	printf("%s", str);
 	return (0);
