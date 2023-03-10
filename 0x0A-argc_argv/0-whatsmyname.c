a0ead404ac3ef9a71f950d21ae8e9ef109e261ba
sundayodu <sundayemmanuelng@yahoo.com>
2023-03-10 15:35:31 +0100

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * main - main function
+  *
+  * @argc: Argument
+  *
+  * @argv: Arguiment
+  *
+  * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
